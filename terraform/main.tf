eeab0b47f3a4249e18ba9bead8639f603c0f6e42
YGplgr <104417941+YGplgr@users.noreply.github.com>
2024-02-25 16:06:17 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,26 @@ provider "aws" {
   region = "us-east-1" # Change this to your desired region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-test-bucket"
-  acl    = "private"
+data "aws_caller_identity" "current" {}
+
+
+locals {
+ account_id = data.aws_caller_identity.current.account_id
+}
+
+
+output "account_id" {
+ value = local.account_id
+}
+
+
+resource "random_id" "rando" {
+ byte_length = 8
+}
+
+
+resource "aws_s3_bucket" "my-bucket" {
+ bucket = "${local.account_id}-my-s3-bucket-${random_id.rando.hex}"
 
   tags = {
     Name                 = "My S3 Bucket"
